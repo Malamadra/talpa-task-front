58d6f0133e990bd626506d97dd2e679bfa35a2a7
Alex Pomazan <multdirectx@gmail.com>
2020-01-22 20:07:07 +0200

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import React from 'react'
+import { Router, Switch, Route } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
 
-const App = () => <div>Test</div>
+const customHistory = createBrowserHistory()
+
+const App = () => (
+  <Router history={customHistory}>
+    <Switch>
+      <Route path="/" exact component={() => <div>Main</div>} />
+      <Route path="/sensor/:id" exact component={() => <div>Sensor data</div>} />
+      <Route component={() => <div>No route</div>} />
+    </Switch>
+  </Router>
+)
 
 export default App
