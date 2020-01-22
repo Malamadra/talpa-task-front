7fc9e47e74a74c4a9d0e431362b4356f819079c8
Alex Pomazan <multdirectx@gmail.com>
2020-01-22 20:38:10 +0200

    graphql added to project

--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { createGlobalStyle } from 'styled-components'
 import App from 'App'
+import { ApolloProvider } from 'react-apollo'
+import { ApolloProvider as ApolloProviderHooks } from '@apollo/react-hooks'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/graphql'
+})
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
 
 const GlobalStyle = createGlobalStyle`  
   html {
@@ -29,7 +43,11 @@ const GlobalStyle = createGlobalStyle`
 const Root = (
   <>
     <GlobalStyle />
-    <App />
+    <ApolloProvider client={client}>
+      <ApolloProviderHooks client={client}>
+        <App />
+      </ApolloProviderHooks>
+    </ApolloProvider>
   </>
 )
 
