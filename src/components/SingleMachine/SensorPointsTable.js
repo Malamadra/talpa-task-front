2d4b1bbbc8495b923b187f8093e5d3ba961f7cc5
Alex Pomazan <multdirectx@gmail.com>
2020-01-26 15:29:44 +0200

    implemented sorting

--- src/components/SingleMachine/SensorPointsTable.js
+++ src/components/SingleMachine/SensorPointsTable.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 import {
   Table,
   TableBody,
@@ -9,7 +9,36 @@ import {
 } from '@material-ui/core'
 import { format } from 'date-fns'
 
+const SORT_DIRECTIONS = {
+  ASC: 'asc',
+  DESC: 'desc'
+}
+
+const sortSensorPoints = (array, sortDirection) => {
+  if (sortDirection) {
+    return array.sort((a, b) =>
+      sortDirection === SORT_DIRECTIONS.ASC
+        ? a.timestamp - b.timestamp
+        : b.timestamp - a.timestamp
+    )
+  }
+
+  return array
+}
+
 const SensorPointsTable = ({ sensorPoints }) => {
+  const [sortDirection, setSortDirection] = useState(null)
+  const sensorPointsToShow = sortSensorPoints(sensorPoints, sortDirection)
+
+  const toggleSortDirection = useCallback(() => {
+    const newDirection =
+      !sortDirection || sortDirection === SORT_DIRECTIONS.ASC
+        ? SORT_DIRECTIONS.DESC
+        : SORT_DIRECTIONS.ASC
+
+    setSortDirection(newDirection)
+  }, [sortDirection])
+
   return (
     <Table aria-label="simple table">
       <TableHead>
@@ -19,8 +48,8 @@ const SensorPointsTable = ({ sensorPoints }) => {
           <TableCell align="right">
             <TableSortLabel
               active
-              direction='asc'
-              onClick={() => {}}
+              direction={sortDirection || SORT_DIRECTIONS.ASC}
+              onClick={toggleSortDirection}
             >
               Date
             </TableSortLabel>
@@ -28,16 +57,14 @@ const SensorPointsTable = ({ sensorPoints }) => {
         </TableRow>
       </TableHead>
       <TableBody>
-        {!!sensorPoints.length &&
-          sensorPoints.map(({ id, value, timestamp }, index) => (
+        {!!sensorPointsToShow.length &&
+          sensorPointsToShow.map(({ id, value, timestamp }, index) => (
             <TableRow key={id}>
               <TableCell component="th" scope="row">
                 {index + 1}
               </TableCell>
               <TableCell align="right">{value}</TableCell>
-              <TableCell align="right">
-                {format(timestamp, 'PP')}
-              </TableCell>
+              <TableCell align="right">{format(timestamp, 'PP')}</TableCell>
             </TableRow>
           ))}
       </TableBody>
